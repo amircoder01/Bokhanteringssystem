4472b230f5620b3d62192faea19c5897ceec911d
99amiqur <99amiqur@gafe.molndal.se>
2025-02-05 10:37:51 +0100

    first

---
+++ Bokhanteringssystem/Bokhanteringssystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bokhanteringssystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
